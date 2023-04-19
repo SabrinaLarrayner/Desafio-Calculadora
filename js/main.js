a753e8b8d0a15e22810ec88b1c71cc2b8d22edd8
Sabrina <sabrinalarrayner2015@outlook.com>
2023-04-18 22:46:05 -0400

    Reajuste no componente Js

--- js/main.js
+++ js/main.js
@@ -1,38 +1,40 @@
 
 document.querySelector('.result').innerHTML = 0
-function buttonMais() {
-    const input1 = parseFloat(document.getElementById('number1').value)
-    const input2 = parseFloat(document.getElementById('number2').value)
-     
-
-    if (isNaN(input1)){
-        document.getElementById('number1').classList.add('input-error')
-        alert('Por favor, preencha os campos com valores numéricos.')
-    } if (isNaN(input2)){
-        document.getElementById('number2').classList.add('input-error')
-        alert('Por favor, preencha os campos com valores numéricos.')
-    }else {
-        document.querySelector('.result').innerHTML  = input1 +  input2
-    }
-} 
-function buttonMultiplicacao() {
-    const input1 = parseFloat(document.getElementById('number1').value)
-    const input2= parseFloat(document.getElementById('number2').value)
-    if (isNaN(input1)){
-        alert('Por favor, preencha os campos com valores numéricos.')
-       Number=  document.getElementById('number1').classList.add('input-error')
-    } if (isNaN(input2)){
-        document.getElementById('number2').classList.add('input-error')
-        alert('Por favor, preencha os campos com valores numéricos.')
-    } else {
-         document.querySelector('.result').innerHTML = input1 * input2
-    }
-} 
-function clearInputs() {
-    const input1 = parseFloat(document.getElementById('number1').value);
-    const input2 = parseFloat(document.getElementById('number2').value);
+function calculate(operador) {  //recebo o operador como parametro da função
+    if (operador == 'C') {
+        let inputClass1 = document.getElementById('number1').classList
+        inputClass1.remove('input-error')
+        let inputClass2 = document.getElementById('number2').classList
+        inputClass2.remove('input-error')
         document.getElementById('number1').value = '';
         document.getElementById('number2').value = '';
-        document.querySelector('.result').innerHTML= 0;
+        document.querySelector('.result').innerHTML = 0;
+    } else {
+        const input1 = parseFloat(document.getElementById('number1').value)
+        const input2 = parseFloat(document.getElementById('number2').value)
+
+        if (isNaN(input1)) {
+            document.getElementById('number1').classList.add('input-error')
+            alert('Por favor, preencha somente com números!')
+        }
+        else if (isNaN(input2)) {
+            document.getElementById('number2').classList.add('input-error')
+            alert('Por favor, preencha somente com números!')
+        }
+        else {
+            let inputClass1 = document.getElementById('number1').classList
+            inputClass1.remove('input-error')
+            let inputClass2 = document.getElementById('number2').classList
+            inputClass2.remove('input-error')
+
+            if (operador == '+') {
+                document.querySelector('.result').innerHTML = input1 + input2
+            }
+
+            if (operador == '*') {
+                document.querySelector('.result').innerHTML = input1 * input2
+            }
+        }
     }
 
+}
